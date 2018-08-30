84f22037b77897c810852f5c62c231dbbb2e1455
Nick <nickjking@me.com>
2018-08-30 16:30:12 +1000

    Save again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-
+// Player controller
 public class PlayerController : MonoBehaviour {
 
     public Slider sliderDivisions;
